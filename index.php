6a4f47d165101179660f4147bc31a626a7882d5c
Pavel <pvlmino@gmail.com>
2020-12-12 13:20:28 +0300

    add index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+	<html lang="ru">
+		<head class="left">
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<meta http-equiv="X-UA-Compatible" content="ie=edge">
+			<title>historysite.com</title>
+		</head>
+		<body>
+			<header class="name">
+				<h1>История России</h1>
+				<h3>Крупнейший портал об истории России</h3>
+				<hr>
+			</header>
+		</body>
+	</html>
