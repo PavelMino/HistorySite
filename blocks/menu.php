b830bf9c35485d48034f348558875c371b045748
Pavel <pvlmino@gmail.com>
2020-12-12 17:53:58 +0300

    Сделал меню, добавил css файлы, изменил главную страницу

---
+++ blocks/menu.php
@@ -0,0 +1,7 @@
+<header class="menu">
+	<a href="index.php" id="name" class="center">История<br>России</a>
+	<a href="profile.php" id="profile"><br>User</a>
+	<a href="#" class="menu-block"><br>Популярно</a>
+	<a href="#" class="menu-block"><br>Поиск</a>
+	<a href="#" class="menu-block"><br>Вопросы</a>
+</header>
--- index.php
+++ index.php
@@ -5,12 +5,11 @@
 			<meta name="viewport" content="width=device-width, initial-scale=1.0">
 			<meta http-equiv="X-UA-Compatible" content="ie=edge">
 			<title>historysite.com</title>
+			<link rel="stylesheet" href="css/reset.css">
+			<link rel="stylesheet" href="css/style.css">
+			<link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300&family=PT+Sans+Caption&display=swap" rel="stylesheet">
 		</head>
 		<body>
-			<header class="name">
-				<h1>История России</h1>
-				<h3>Крупнейший портал об истории России</h3>
-				<hr>
-			</header>
+			<?php require "blocks/menu.php" ?>
 		</body>
 	</html>
